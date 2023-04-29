8dd4d0cfd18c6f940768f9294c9aa383d3999f46
Prosantaroy1 <royprosanta172@gmail.com>
2023-04-30 03:06:55 +0600

    3 step server

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const cors = require('cors')
 const port = process.env.PORT || 5000;
 
 const category = require('./data/category.json');
+const news = require('./data/news.json');
+
 
 app.use(cors());
 
@@ -14,6 +16,29 @@ app.get('/', (req, res) => {
 app.get('/category', (req, res)=>{
     res.send(category)
 });
+//All news
+app.get('/news', (req, res)=>{
+    res.send(news)
+})
+//News id
+app.get('/news/:id', (req, res)=>{
+    const id = req.params.id;
+    //console.log(id);
+    const selectedNews = news.find(n=> n._id ===id)
+    res.send(selectedNews)
+})
+//category id News
+app.get('/category/:id', (req, res)=>{
+    const id = parseInt(req.params.id);
+    //console.log(id);
+    if(id===0){
+        res.send(news)
+    }
+    else{
+     const categoryNews = news.filter(n=> parseInt(n.category_id) === id)
+      res.send(categoryNews)
+    }
+})
 
 app.listen(port, () => {
   console.log(`Dragon API is running on port: ${port}`)
